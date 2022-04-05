e9407e6e183fc6814ae3ffe53d664e4dcf0a5ce4
JosephShin <joespam9999@gmail.com>
2022-04-05 12:01:23 -0400

    commit tree 916706cceed425c7f9c9dfd35d385d354f6eb9fe

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(variable){
+    return cats.push(variable);
+}
+
+
+function destructivelyPrependCat(variable){
+    return cats.unshift(variable)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(variable){
+    let newArray = [...cats];
+    newArray.push(variable);
+    return newArray;
+}
+
+function prependCat(variable){
+    let newArray = [...cats];
+    newArray.unshift(variable);
+    return newArray;
+}
+
+function removeLastCat(){
+    let newArray = [...cats];
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat(){
+    let newArray = [...cats];
+    newArray.shift();
+    return newArray;
+} 
